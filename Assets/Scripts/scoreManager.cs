895ea0acf445e4296686f1b8a9ed898ac6db27ed
Dudin-Sasha <sasha_dudin_2007@mail.ru>
2026-04-28 12:55:14 +0200

    2nd stage 2nd commit unity

--- Assets/Scripts/scoreManager.cs
+++ Assets/Scripts/scoreManager.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class scoreManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI speedometr;
+    [SerializeField] private TextMeshProUGUI xp;
+    [SerializeField] private PlayerSc pl;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void upd(float speed = 0, float score = 0) {
+        xp.text = $"your score is {score}";
+        speedometr.text = $"{speed}";
     }
 
     // Update is called once per frame
